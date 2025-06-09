75381a9244b1bc3cfdcec9b84cd7af2b0650488e
Sinemis Toktaş <102419519+sinemistoktas@users.noreply.github.com>
2025-06-10 00:18:38 +0300

    added data class to store info we get from weather API

--- app/src/main/java/com/example/weatherapp/model/WeatherResponse.kt
+++ app/src/main/java/com/example/weatherapp/model/WeatherResponse.kt
@@ -2,7 +2,30 @@ package com.example.weatherapp.model
 
 /**
  * Data class that models the JSON response we get from the API
+ * Used multiple nested classes to match the JSON response from API
+ * Open-Meteo API Source: https://open-meteo.com/en/docs#api-form
  */
 
-class WeatherResponse {
-}
+data class WeatherResponse(
+    val current_weather: CurrentWeather?,
+    val hourly: HourlyData?,
+    val daily: DailyData?
+)
+
+data class CurrentWeather(
+    val temperature: Double?,
+    val windspeed: Double?,
+    val weathercode: Int?,
+    val time: String?
+)
+
+data class HourlyData(
+    val precipitation_probability: List<Double>?,
+    val relative_humidity_2m: List<Double>?
+)
+
+data class DailyData(
+    val temperature_2m_max: List<Double>?,
+    val temperature_2m_min: List<Double>?
+)
+
